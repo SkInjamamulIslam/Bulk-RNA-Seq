2775bbb540cff5cd89eb544278a2c51176afb8a6
Khandker Shahed <71299728+khandkershahed@users.noreply.github.com>
2025-08-13 09:55:05 +0600

    Update Shell Script 1

--- script1.sh
+++ script1.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 # Set the path to the input directory as the current directory
 input_dir=$(pwd)
 
@@ -7,8 +6,6 @@ input_dir=$(pwd)
 for file in "$input_dir"/*.fastq; do
     # Extract the filename without the extension
     filename=$(basename "$file" .fastq)
-
     # Run the trimmomatic command on the file
     java -jar trimmomatic-0.39.jar SE -threads 32 "$file" "$input_dir/$filename"_trimmed.fastq TRAILING:10 -phred33 
 done
-
